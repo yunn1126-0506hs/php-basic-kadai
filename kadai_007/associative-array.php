3c60d8ec2da31c0330af2670b4a5273e49b49b83
yunn1126-0506hs <tko38ksmy38@gmail.com>
2024-12-15 21:48:20 +0900

    kadai_007ディレクトリの追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHPの基礎を学ぼう</title>
+
+</head>
+<body>
+  <?php
+    $product_date = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    print_r($product_date);
+
+  ?>
+</body>
+</html>
